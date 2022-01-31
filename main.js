5d718630ce3d2566fc7f3d01dcd32ecb823977ac
firefist10 <kalradev@outlook.com>
2022-01-31 16:56:06 +0530

    open dialog when load

--- main.js
+++ main.js
@@ -1,27 +1,38 @@
-var navLinks = document.getElementsByClassName('nav-link')
+var navLinks = document.getElementsByClassName("nav-link");
 
 // remove active class from every nav-link
-var removeClass = function() {
-    for(let j=0;j<navLinks.length; j++) {
-        navLinks[j].classList.remove('active')
-    }
-}
+var removeClass = function () {
+  for (let j = 0; j < navLinks.length; j++) {
+    navLinks[j].classList.remove("active");
+  }
+};
 
 // function that will add active class based on location hash
-var addActiveClass = function() {
-    console.log(window.location.hash)
-    if(window.location.hash === undefined || window.location.hash ==='') {
-        removeClass();
-        navLinks[0].classList.add('active');
-    } else {
-        removeClass();
-        for(let j=1;j<navLinks.length; j++) {
-            if(navLinks[j].attributes.href.nodeValue === window.location.hash) navLinks[j].classList.add('active')
-        }
+var addActiveClass = function () {
+  console.log(window.location.hash);
+  if (window.location.hash === undefined || window.location.hash === "") {
+    removeClass();
+    navLinks[0].classList.add("active");
+  } else {
+    removeClass();
+    for (let j = 1; j < navLinks.length; j++) {
+      if (navLinks[j].attributes.href.nodeValue === window.location.hash)
+        navLinks[j].classList.add("active");
     }
-}
+  }
+};
 
 // init active class
-addActiveClass()
+addActiveClass();
 // change active nav link based on hashchange
-window.onhashchange = addActiveClass
+window.onhashchange = addActiveClass;
+
+// onload show modal
+var myModal = new bootstrap.Modal(document.getElementById("exampleModal"), {
+  keyboard: false,
+});
+
+window.addEventListener("load", () => {
+  console.log("asdf");
+  myModal.toggle();
+});
